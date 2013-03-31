02e187cdc78e6239ce576beecfa96b091bc34ad1
bendem <bendembd@gmail.com>
2013-03-31 12:03:14 +0200

    Admin creation

--- ctrl/adminCtrl.php
+++ ctrl/adminCtrl.php
@@ -2,9 +2,27 @@
 
 class adminCtrl extends Ctrl {
 
+	public $uses = array(
+		'Models' => array('User', 'Ticket')
+	);
 	public $allowed = 'admin';
 
 	public function admin() {
+		$user = $this->Session->read('user');
+		$tickets = $this->Ticket->search(array(
+				'user_id' => $user->id,
+				'closed' => 0
+			),
+			array(
+				'limit' => '4',
+				'order' => array(
+					'field' => 'date',
+					'order' => 'desc'
+				)
+			)
+		);
+
+		$this->set('tickets', $tickets);
 	}
 
 
--- views/admin/admin.php
+++ views/admin/admin.php
@@ -1,1 +1,20 @@
-<h2>Administration</h2>
+<section class="middle tickets">
+	<h2>Tickets récents</h2>
+	<?php foreach($tickets as $ticket): ?>
+		<article>
+			<h3><?= ucfirst($ticket->subject) ?></h3>
+			<div class="msg">
+				<?= $ticket->content ?>
+			</div>
+			<div class="infos">
+				<ul>
+					<li><?= $ticket->date ?></li>
+					<li><?= ucfirst($ticket->type) ?></li>
+				</ul>
+			</div>
+		</article>
+	<?php endforeach; ?>
+</section>
+<section class="middle ">
+	<h2>Derniers inscrits</h2>
+</section>
