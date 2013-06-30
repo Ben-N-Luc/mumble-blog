d4502a94c1b3c41774e40a7ce646c88af7114f83
Quelu <quelumatagne@gmail.com>
2013-06-30 20:43:30 +0200

    Gestion de l'inscription
    Inscription avec utilisation du helper Form et gestion des erreurs

--- ctrl/connexionCtrl.php
+++ ctrl/connexionCtrl.php
@@ -11,69 +11,56 @@ class connexionCtrl extends Ctrl {
 		'inscription' => array('username', 'email', 'pwd', )
 	);
 
-	public function connexion() {
-
-		// Post
-		if($this->Request->posted) {
-			// Connexion
-			if(object_keys($this->Request->post) == $this->champs['connexion']) {
+	public function connexion() 
+	{
+		if ($this->Request->posted) 
+		{
+			if ($this->Request->post->action == "sign") 
+			{
 				$user = current($this->User->search(
 					array('pseudo' => $this->Request->post->pseudo)
 				));
-				if($user) {
-					if(sha1($this->Request->post->mdp) == $user->password) {
-						unset($user->password);
-						$this->Session->auth($user);
-						$this->Session->setFlash('Vous êtes bien connecté', 'success');
-						$this->redirect(url());
-					} else {
-						$this->Session->setFlash('Mot de passe incorrect', 'error');
-					}
-				} else {
-					$this->Session->setFlash('Pseudo non reconnu', 'error');
-				}
-
-			// Inscription
-			} elseif(object_keys($this->Request->post) == $this->champs['inscription']) {
-
-				// déjà un utilisateurs ?
-				$user = current($this->User->search(
-					array('pseudo' => $this->Request->post->username, 'mail' => $this->Request->post->email),
-					array('logical_term' => 'OR')
+				$mail = current($this->User->search(
+					array('mail' => $this->Request->post->mail)				
 				));
-				if($user) {
-					$this->Session->setFlash('Pseudo ou adresse mail déjà pris', 'warning');
-				} else {
-					// email valide ?
-					if (filter_var($this->Request->post->email, FILTER_VALIDATE_EMAIL)) {
 
-						// longueur mdp
-						if (strlen($this->Request->post->pwd) < 6) {
-							$this->Session->setFlash('Votre mot de passe doit faire au moins 6 caractères');
-						} else {
-							// écriture en bdd
-							$data = array(
-								'pseudo' => $this->Request->post->username,
-								'password' => sha1($this->Request->post->pwd),
-								'mail' => $this->Request->post->email
-							);
-							if($this->User->add($data)) {
-								$this->Session->setFlash('Vous êtes bien inscrit', "success");
-							} else {
-								$this->Session->setFlash('Erreur interne, réessayez plus tard', 'error');
-								$this->Log->add(array(
-									'message' => "Erreur interne lors de l'inscription sur la requête " . $this->User->lastRequest,
-									'token' => 'internal error'
-								));
-							}
+				if($user)
+				{
+					$this->Helper->errors['pseudo'] = 'Pseudo déjà utilisé';
+				} 
+				if($mail)
+				{
+					$this->Helper->errors['mail'] = 'Mail déjà utilisé';
+				} 
+				elseif(!$mail && !$user) 
+				{
+					if ($this->User->validates($this->Request->post)) 
+					{
+						if ($this->User->add($data)) 
+						{
+							$this->Session->setFlash('Vous êtes bien inscrit', 'success');
+						} 
+						else 
+						{
+							$this->Session->setFlash('Erreur interne, réessayez plus tard', 'error');
+							$this->Log->add(array(
+								'message' => "Erreur interne lors de l'inscription sur la requête " . $this->User->lastRequest,
+								'token' => 'internal error'
+							));
 						}
-					} else {
-						$this->Session->setFlash('Adresse Email invalide', 'warning');
+						
+					} 
+					else 
+					{
+						$this->Session->setFlash('Erreur, vérifiez vos informations', 'error');
 					}
+					
 				}
-
+			} 
+			else 
+			{
+				# code...
 			}
-		}
-
+		} 
 	}
 }
--- views/connexion/connexion.php
+++ views/connexion/connexion.php
@@ -1,20 +1,16 @@
 <section class="middle">
 	<h2>Inscription</h2>
-	<form action="<?php echo url(REQUEST_URI) ?>" method="post">
-		<label for="username">Pseudo</label>
-		<input type="text" id="username" name="username" required>
-		<label for="email">Email</label>
-		<input type="email" id="email" name="email" required>
-		<label for="pwd">Mot de passe</label>
-		<input type="password" id="pwd" name="pwd" required>
-		<div class="actions">
-			<input type="submit" value="Valider" class="btn">
-		</div>
-	</form>
+	<?= $this->Form->startForm(url(REQUEST_URI)) ?>
+	<?= $this->Form->input('action', 'hidden') ?>
+	<?= $this->Form->input('pseudo', 'Pseudo') ?>
+	<?= $this->Form->input('mail', 'Email', array('type' => 'mail')) ?>
+	<?= $this->Form->input('password', 'Mot de passe', array('type' => 'password')) ?>
+	<?= $this->Form->submit("S'inscrire", array('class' => 'btn')) ?>
+	<?= $this->Form->endForm() ?>
 </section>
-<section class="middle">
+<!-- <section class="middle">
 	<h2>Connexion</h2>
-	<form action="<?php echo url(REQUEST_URI) ?>" method="post">
+	<form action="<?php //echo url(REQUEST_URI) ?>" method="post">
 		<label for="pseudo">Pseudo</label>
 		<input type="text" id="pseudo" name="pseudo" required>
 		<label for="mdp">Mot de passe</label>
@@ -24,4 +20,4 @@
 		</div>
 	</form>
 </section>
-<div class="clearfix"></div>
+<div class="clearfix"></div> -->
