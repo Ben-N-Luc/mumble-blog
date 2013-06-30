f8dc483194e720aff060d04042b2d450cd81bc0a
Quelu <quelumatagne@gmail.com>
2013-06-30 22:53:01 +0200

    Formulaire de connexion
    Réécriture du formulaire de connexion avec le helper

--- views/connexion/connexion.php
+++ views/connexion/connexion.php
@@ -1,3 +1,4 @@
+<section class="middle">
 	<h2>Inscription</h2>
 	<?= $this->Form->startForm(url(REQUEST_URI)) ?>
 	<?= $this->Form->input('action', 'hidden', array('value' => 'signup')) ?>
@@ -6,16 +7,14 @@
 	<?= $this->Form->input('password', 'Mot de passe', array('type' => 'password')) ?>
 	<?= $this->Form->submit("S'inscrire", array('class' => 'btn')) ?>
 	<?= $this->Form->endForm() ?>
-<!-- <section class="middle">
-	<h2>Connexion</h2>
-	<form action="<?php //echo url(REQUEST_URI) ?>" method="post">
-		<label for="pseudo">Pseudo</label>
-		<input type="text" id="pseudo" name="pseudo" required>
-		<label for="mdp">Mot de passe</label>
-		<input type="password" id="mdp" name="mdp" required>
-		<div class="actions">
-			<input type="submit" value="Valider" class="btn">
-		</div>
-	</form>
 </section>
-<div class="clearfix"></div> -->
+
+<section class="middle">
+	<h2>Connexion</h2>
+	<?= $this->Form->startForm(url(REQUEST_URI)) ?>
+	<?= $this->Form->input('action', 'hidden', array('value' => 'signin')) ?>
+	<?= $this->Form->input('pseudo', 'Pseudo') ?>
+	<?= $this->Form->input('password', 'Mot de passe', array('type' => 'password')) ?>
+	<?= $this->Form->submit("Connexion", array('class' => 'btn')) ?>
+	<?= $this->Form->endForm() ?>
+</section>
