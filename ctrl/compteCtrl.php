c861047c5c38ca4c4e177de35cd0588501468900
Quelu <quelumatagne@gmail.com>
2013-07-01 01:04:30 +0200

    Avancement changement de mot de passe
    Voir pour la gestion d'affichage de message d'erreur

--- ctrl/compteCtrl.php
+++ ctrl/compteCtrl.php
@@ -30,6 +30,38 @@ class compteCtrl extends Ctrl {
 	}
 
 	public function edit() {
+		$user = $this->Session->read('user');
+		$user = current($this->User->search(array(
+			'id'	=> $user->id
+		)));
+
+		if ($this->Request->post) {
+			// Changement de mot de passe
+			if ($this->Request->post->action == "pass_edit") {
+				unset($this->Request->post->action);
+				if ($user->password == sha1($this->Request->post->old_password)) {
+					if ($this->User->validates($this->Request->post) && $this->Request->post->password1 == $this->Request->post->password2) {
+						$new_user->password = $this->Request->post->password1;
+						$this->User->update(array('id' => $user->id), $new_user);
+						$this->Request->post = new stdClass();
+						$this->Session->setFlash('Mot de passe modifié avec succès !', 'success');
+					}
+					else {
+						$this->User->errors['password2'] = 'Les deux mots de passe doivent être identique';
+						$this->Session->setFlash('Erreur !', 'error');
+					}
+				}
+				else {
+					$this->User->errors['old_password'] = 'Le mot de passe est incorrect.';
+					$this->Session->setFlash('Erreur !', 'error');
+				}
+			}
+			else {
+				// Changement d'email
+			}
+		}
+
+		$this->set('email' , $user->mail);
 	}
 
 	public function deconnexion() {
