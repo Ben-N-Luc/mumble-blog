42831f82d092bef85db329ce97d6cee5bb41e84e
bendem <bendembd@gmail.com>
2013-07-01 12:16:13 +0200

    Déplacement du core dans le dossier Core

--- libs/Includes.php
+++ libs/Includes.php
@@ -1,10 +1,18 @@
 <?php
 
-$exclude = array('.', '..');
+$exclude = array('.', '..', 'Core');
+
+$dir = opendir(LIBS_DIR . DS . 'Core');
+while ($file = readdir()) {
+	if(!in_array($file, $exclude)) {
+		require LIBS_DIR . DS . 'Core' . DS . $file;
+	}
+}
+closedir($dir);
 
 $dir = opendir(LIBS_DIR);
 while ($file = readdir()) {
 	if($file != basename(__FILE__) && !in_array($file, $exclude)) {
-		require_once LIBS_DIR . DS . $file;
+		require LIBS_DIR . DS . $file;
 	}
 }
