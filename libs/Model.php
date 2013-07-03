4e397c0b6904e6c0b612f8bef2c75733c90c463c
bendem <bendembd@gmail.com>
2013-07-03 14:12:32 +0200

    Amélioration de la validation

--- libs/Model.php
+++ libs/Model.php
@@ -12,26 +12,24 @@ class Model extends AppModel {
 		$errors = array();
 
 		foreach ($this->validate as $k => $v) {
-			if (!isset($data->$k)) {
-				// $errors[$k] = $v['message'];
-			} else {
-				if ($v['rule'] == 'notEmpty' && empty($data->$k)) {
-					$errors[$k] = $v['message'];
-				}
-				if (isset($v['verif'])) {
-					if ($v['verif'] == 'regex' && !preg_match('/^' . $v['rule'] . '$/', $data->$k)) {
-						$errors[$k] = $v['message'];
-					} elseif ($v['verif'] == 'filtre') {
-						if ($v['rule'] == 'mail' && !filter_var($data->$k, FILTER_VALIDATE_EMAIL)) {
-							$errors[$k] = $v['message'];
-						} elseif ($v['rule'] == 'url' && !preg_match('`((?:https?|ftp)://\S+[[:alnum:]]/?)`si',$url)) {
-							$errors[$k] = $v['message'];
-						}
+			if (isset($data->$k)) {
+				if ($v['verif'] == 'notEmpty' && empty($data->$k)) {
+					$this->errors[$k] = $v['message'];
+				} elseif ($v['verif'] == 'regex' && !preg_match('/^' . str_replace('/', '\/', $v['rule']) . '$/', $data->$k)) {
+					$this->errors[$k] = $v['message'];
+				} elseif ($v['verif'] == 'filtre') {
+					if ($v['rule'] == 'mail' && !filter_var($data->$k, FILTER_VALIDATE_EMAIL)) {
+						$this->errors[$k] = $v['message'];
+					} elseif ($v['rule'] == 'url' && !preg_match('`(https?|ftp)://[a-z0-9][a-z0-9./_-]*(\?[a-z0-9./_%-]+=[a-z0-9./_%-]+(&[a-z0-9./_%-]+=[a-z0-9./_%-])*)?(#[a-z0-9./_%-])?`', $url)) {
+						$this->errors[$k] = $v['message'];
+					}
+				} elseif($v['verif'] == 'in_array') {
+					if(!in_array($data->$k, $v['rule'])) {
+						$this->errors[$k] = $v['message'];
 					}
 				}
 			}
 		}
-		$this->errors = $errors;
 
 		return empty($errors);
 	}
--- model/User.php
+++ model/User.php
@@ -3,22 +3,27 @@
 class User extends Model {
 	var $validate = array(
 		'pseudo'	=> array(
-			'rule'		=> 'notEmpty',
-			'message'	=> 'Le pseudo est obligatoire'
+			'verif'	  => 'notEmpty',
+			'message' => 'Le pseudo est obligatoire'
 		),
 		'pseudo'	=> array(
-			'rule'		=> 'notEmpty',
-			'message'	=> 'Le pseudo est obligatoire'
+			'verif'	  => 'notEmpty',
+			'message' => 'Le pseudo est obligatoire'
 		),
 		'password'	=> array(
-			'verif' 	=> 'regex',
-			'rule'		=> '.{6,16}',
-			'message'	=> 'Votre mot de passe doit faire entre 6 et 16 caractères'
+			'verif'   => 'regex',
+			'rule'	  => '.{6,16}',
+			'message' => 'Votre mot de passe doit faire entre 6 et 16 caractères'
 		),
 		'mail'		=> array(
-			'verif' 	=> 'filtre',
-			'rule'		=> 'mail',
-			'message'	=> "L'email entré est incorrect"
+			'verif'   => 'filtre',
+			'rule'	  => 'mail',
+			'message' => "L'email entré est incorrect"
+		),
+		'rank'      => array(
+			'verif'   => 'in_array',
+			'rule'    => array('a', 'u'),
+			'message' => 'Le rang doit être "a" ou "u"'
 		)
 	);
 }
