a921e7e1216dc3fb802513e5f2e2e9e72f8d5381
bendem <bendembd@gmail.com>
2013-07-05 13:13:41 +0200

    Stylisation du formulaire...

--- views/compte/compte.php
+++ views/compte/compte.php
@@ -3,8 +3,8 @@
 	<div class="account">
 			<?php $user = $this->Session->read('user') ?>
 			<?= $this->Form->start(null, array('enctype' => 'multipart/form-data', 'alignment' => 'right')) ?>
-			<?= $this->Form->input('avatar', array('type' => 'file', 'file_type' => 'avatar')) ?>
-			<?= $this->Form->submit('Modifier') ?>
+			<?= $this->Form->input('avatar', array('type' => 'file', 'file_type' => 'avatar', 'required' => 'required')) ?>
+			<?= $this->Form->submit("Modifier l'avatar") ?>
 			<?= $this->Form->end() ?>
 		<ul>
 			<li>
