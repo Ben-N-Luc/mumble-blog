7f885f830d5abbe63c648dab8223c1b007d4cb91
bendem <bendembd@gmail.com>
2013-07-27 11:30:21 +0200

    Ajout des avatars aux posts

--- views/ticket/ticket.php
+++ views/ticket/ticket.php
@@ -9,40 +9,52 @@
 	<?php endif ?>
 	<?php foreach($tickets as $ticket): ?>
 		<article>
-			<h3>
-				<a href="<?= url('ticket/view/' . $ticket->id) ?>">
-					<?= $ticket->subject ?>
+			<div class="account left">
+				<a href="<?= url('ticket/ticket/' . $ticket->user_id) ?>">
+					<img src="<?= url('img/users/' . $this->getAvatarName($ticket->user_id)) ?>">
 				</a>
-				<small style="vertical-align: middle">
-					<a href="<?= url('ticket/ticket/' . (($ticket->closed) ? 'closed' : 'opened')) ?>" class="badge badge-<?= ($ticket->closed) ? 'important' : 'success' ?>">
-						<?= ($ticket->closed) ? 'Fermé' : 'Ouvert' ?>
+				<div>
+					<a href="<?= url('ticket/ticket/' . $ticket->user_id) ?>">
+						<?= $ticket->pseudo ?>
 					</a>
-				</small>
-			</h3>
-			<div class="msg">
-				<?= $ticket->content ?>
+				</div>
 			</div>
-			<div class="infos">
-				<ul>
-					<li>Utilisateur : <a href="<?= url('ticket/ticket/' . $ticket->user_id) ?>"><?= $ticket->pseudo ?></a></li>
-					<li>Réponses : <?= $ticket->answers ?></li>
-					<li><?= $ticket->date ?></li>
-					<li><?= $this->badge($ticket->type) ?></li>
-					<li>
-						<a href="<?= url('ticket/view/' . $ticket->id) ?>" class="btn">
-							Voir le ticket
+			<div class="ticket-content right">
+				<h3>
+					<a href="<?= url('ticket/view/' . $ticket->id) ?>">
+						<?= $ticket->subject ?>
+					</a>
+					<small style="vertical-align: middle">
+						<a href="<?= url('ticket/ticket/' . (($ticket->closed) ? 'closed' : 'opened')) ?>" class="badge badge-<?= ($ticket->closed) ? 'important' : 'success' ?>">
+							<?= ($ticket->closed) ? 'Fermé' : 'Ouvert' ?>
 						</a>
-					</li>
-					<li>
-						<?php if ($ticket->closed): ?>
-							<a href="<?= url('ticket/close/' . $ticket->id) ?>" class="btn btn-success">Rouvrir</a>
-						<?php else: ?>
-							<a href="<?= url('ticket/close/' . $ticket->id) ?>" class="btn btn-danger">
-								Fermer
+					</small>
+				</h3>
+				<div class="msg">
+					<?= $ticket->content ?>
+				</div>
+				<div class="infos">
+					<ul>
+						<li>Utilisateur : <a href="<?= url('ticket/ticket/' . $ticket->user_id) ?>"><?= $ticket->pseudo ?></a></li>
+						<li>Réponses : <?= $ticket->answers ?></li>
+						<li><?= $ticket->date ?></li>
+						<li><?= $this->badge($ticket->type) ?></li>
+						<li>
+							<a href="<?= url('ticket/view/' . $ticket->id) ?>" class="btn">
+								Voir le ticket
 							</a>
-						<?php endif ?>
-					</li>
-				</ul>
+						</li>
+						<li>
+							<?php if ($ticket->closed): ?>
+								<a href="<?= url('ticket/close/' . $ticket->id) ?>" class="btn btn-success">Rouvrir</a>
+							<?php else: ?>
+								<a href="<?= url('ticket/close/' . $ticket->id) ?>" class="btn btn-danger">
+									Fermer
+								</a>
+							<?php endif ?>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</article>
 	<?php endforeach; ?>
